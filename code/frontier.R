c696c5b765b6c1cf5dac2080e8d8ddfaeaa27968
timelyportfolio <kent.russell@timelyportfolio.com>
2016-11-08 14:58:52 -0600

    add animated plotly rolling frontier code

--- code/frontier.R
+++ code/frontier.R
@@ -64,3 +64,68 @@ frontier@portfolio@portfolio$weights %>>%
       add_markers(y = ~weights[,"BofAMLCorp"], name="BofAMLCorp")
   }) %>>%
   layout(hovermode = "x")
+
+
+# now attempt a rolling frontier with plotly animation
+SPEC = portfolioSpec()
+setTargetReturn(SPEC) = 0
+CONSTRAINTS = "LongOnly"
+
+from = seq(1975, 2010, by = 5)
+to = from + 5
+
+rf <- returns_xts %>>%
+  na.omit() %>>%
+  as.timeSeries() %>>%
+  {
+    rollingPortfolioFrontier(
+      data = .,
+      spec = SPEC,
+      constraints = CONSTRAINTS,
+      from = timeDate(paste0(from,"-12-31")),
+      to = timeDate(paste0(to,"-12-31"))
+    )
+  }
+
+rf %>%
+  map_df(
+    function(frontier){
+      frontierPoints(frontier) %>>%
+        data.frame(
+          SP500 = frontier@portfolio@portfolio$weights[,2],
+          BofAMLCorp = frontier@portfolio@portfolio$weights[,1],
+          date = getSeries(frontier@data) %>>%
+            tail(1) %>>%
+            rownames() %>>%
+            as.character,
+          stringsAsFactors = FALSE
+        )
+    }
+  ) %>>%
+  plot_ly(x=~targetRisk, y=~targetReturn) %>>%
+  add_markers(
+    text = ~sprintf(
+      "S&P 500: %.1f%%<br />BofA ML Corp: %.1f%%<br />Return: %.2f%%<br />Risk: %.2f%%",
+      SP500 * 100,
+      BofAMLCorp * 100,
+      targetReturn * 100,
+      targetRisk * 100
+    ),
+    color = ~date, 
+    hoverinfo = "text",
+    frame = ~date,
+    showlegend = FALSE
+  ) %>>%
+  layout(
+    title = "<b>Efficent Frontier</b><br>Rolling 5 Year",
+    margin = 100
+  ) %>>%
+  animation_opts( frame = 2000, transition = 1000 )
+
+
+upload_plotly <- function(p){
+  Sys.setenv("plotly_username" = "timelyportfolio")
+  Sys.setenv("plotly_api_key" = "3f9xw96gq0")
+  
+  plotly_POST(p)
+}
